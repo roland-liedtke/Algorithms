d85dcb43f376260794b11a44d1b0a1d757cc846a
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-11 17:55:41 +0200

    Update linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -6,3 +6,12 @@ function linearSearch(arr, targetValue) {
   }  
   return null;
 }
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = linearSearch(primes, 73);
+println("Found prime at index " + result);
+
+Program.assertEqual(linearSearch(primes, 73), 20);
+Program.assertEqual(linearSearch(primes, 3), 1);
+Program.assertEqual(linearSearch(primes, 7), 3);
