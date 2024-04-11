c17134067ec6fc5b19703d16efc07c4298cd62e1
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-11 17:50:06 +0200

    Create binary_search.js

---
+++ binary_search.js
@@ -0,0 +1,31 @@
+var doSearch = function(array, targetValue) {
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+    var shoot = 1;
+    while(max >= min){
+        guess = Math.floor((max + min) / 2);
+        println(guess);
+        if(array[guess] === targetValue){
+            println(shoot);
+            return guess;
+        }
+        else if(array[guess] < targetValue){
+            min = guess + 1;
+            shoot++;
+        }
+        else{
+            max = guess - 1;
+        }
+    }
+    return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+println("Found prime at index " + result);
+
+Program.assertEqual(doSearch(primes, 73), 20);
+Program.assertEqual(doSearch(primes, 3), 1);
+Program.assertEqual(doSearch(primes, 7), 3);
