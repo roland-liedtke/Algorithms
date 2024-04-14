cd52d286ad4be6a24b1ae5cf0174473b97d068dd
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 14:36:20 +0200

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,13 +1,12 @@
-function doSearch(arr, targetValue) {
+function binarySearch(arr, targetValue) {
     var min = 0;
     var max = arr.length - 1;
     var guess;
     var shoot = 1;
     while(max >= min){
         guess = Math.floor((max + min) / 2);
-        println(guess);
         if(arr[guess] === targetValue){
-            println(shoot);
+            console.log(shoot);
             return guess;
         }
         else if(arr[guess] < targetValue){
@@ -23,9 +22,5 @@ function doSearch(arr, targetValue) {
 
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-var result = doSearch(primes, 73);
-println("Found prime at index " + result);
-
-Program.assertEqual(doSearch(primes, 73), 20);
-Program.assertEqual(doSearch(primes, 3), 1);
-Program.assertEqual(doSearch(primes, 7), 3);
+var result = binarySearch(primes, 73);
+console.log("Found prime at index " + result);    // should be 20!
