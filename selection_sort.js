55007efe83d6aece467eac5c2a318c57408892ba
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 18:38:37 +0200

    Update selection_sort.js

--- selection_sort.js
+++ selection_sort.js
@@ -1,12 +1,8 @@
-var indexOfMinimum = function(arr, startIndex) {
-    // Set initial values for minValue and minIndex,
-    // based on the leftmost entry in the subarray:  
-    var minValue = arr[startIndex];
+function indexOfMinimum(arr, startIndex) {
+    var minValue = arr[startIndex];    // Set initial values for minValue and minIndex
     var minIndex = startIndex;
 
-    // Loop over items starting with startIndex, 
-    // updating minValue and minIndex as needed:
-    for(var i = minIndex + 1; i < arr.length  ; i++ ) {
+    for(var i = minIndex + 1; i < arr.length; i++) {
         if( arr[i] < minValue ) {
             minIndex = i;
             minValue = arr[i];
@@ -16,13 +12,7 @@ var indexOfMinimum = function(arr, startIndex) {
     return minIndex;
 }; 
 
-var array = [18, 6, 66, 44, 9, 22, 14];   
-var index = indexOfMinimum(array, 2);
-
-//  For the test array [18, 6, 66, 44, 9, 22, 14], 
-//  the value 9 is the smallest of [..66, 44, 9, 22, 14]
-//  Since 9 is at index 4 in the original array, 
-//  "index" has value 4
-println("The index of the minimum value of the subarray starting at index 2 is " + index + "."  );
-Program.assertEqual(index, 4);
+var array = [18, 6, 66, 44, 9, 22, 14];   //  For the test array [18, 6, 66, 44, 9, 22, 14]
+var index = indexOfMinimum(array, 2);    //  the value 9 is the smallest of [..66, 44, 9, 22, 14]  Since 9 is at index 4 in the original array, "index" has value 4
+println("The index of the minimum value of the subarray starting at index 2 is " + index + "."  ); // Should be 4 !
 
