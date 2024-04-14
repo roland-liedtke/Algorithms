49cbb9e7904102d4234fd5c2896ecaf5b8545828
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 18:41:05 +0200

    Update swap.js

--- swap.js
+++ swap.js
@@ -1,10 +1,11 @@
+var testArray = [7, 9, 4];
+
 function swap(arr, firstIndex, secondIndex) {
     var temp = arr[firstIndex];
 	arr[firstIndex] = arr[secondIndex];
 	arr[secondIndex] = temp;
 };
 
-var testArray = [7, 9, 4];
 swap(testArray, 0, 1);
 
 console.log(testArray);    // Should be [9, 7, 4] !
