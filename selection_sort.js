04e807de1fe446499aca1918dd2c4eb5db8549b1
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 18:45:59 +0200

    Update selection_sort.js

--- selection_sort.js
+++ selection_sort.js
@@ -1,5 +1,7 @@
+var array = [18, 6, 66, 44, 9, 22, 14];
+
 function indexOfMinimum(arr, startIndex) {
-    var minValue = arr[startIndex];    // Set initial values for minValue and minIndex
+    var minValue = arr[startIndex];
     var minIndex = startIndex;
 
     for(var i = minIndex + 1; i < arr.length; i++) {
@@ -8,11 +10,9 @@ function indexOfMinimum(arr, startIndex) {
             minValue = arr[i];
         }
     } 
-
+    
     return minIndex;
 }; 
 
-var array = [18, 6, 66, 44, 9, 22, 14];   //  For the test array [18, 6, 66, 44, 9, 22, 14]
-var index = indexOfMinimum(array, 2);    //  the value 9 is the smallest of [..66, 44, 9, 22, 14]  Since 9 is at index 4 in the original array, "index" has value 4
-println("The index of the minimum value of the subarray starting at index 2 is " + index + "."  ); // Should be 4 !
-
+var index = indexOfMinimum(array, 2);
+console.log("The index of the minimum value of the subarray starting at index 2 is " + index + "."  ); // Should be 4 !
