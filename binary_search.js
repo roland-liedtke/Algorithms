ed0ed426533a1e880587453f2fd7e1fb2f5cf734
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 18:40:09 +0200

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,3 +1,5 @@
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
 function binarySearch(arr, targetValue) {
     var min = 0;
     var max = arr.length - 1;
@@ -19,8 +21,6 @@ function binarySearch(arr, targetValue) {
     }
     return -1;
 };
-
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
 var result = binarySearch(primes, 73);
 console.log("Found prime at index " + result);    // should be 20!
