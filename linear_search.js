196befc8dc095a287d102e304be4d74c1c88fc80
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 18:39:34 +0200

    Update linear_search.js

--- linear_search.js
+++ linear_search.js
@@ -1,3 +1,5 @@
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
 function linearSearch(arr, targetValue) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === targetValue) {
@@ -6,8 +8,6 @@ function linearSearch(arr, targetValue) {
   }
   return null;
 }
-
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
 var result = linearSearch(primes, 73);
 console.log("Found prime at index " + result);  // should be 20!
