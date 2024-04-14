81e05aeae7133f9ffbf8ee16b79aef7656074b7b
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-14 14:38:47 +0200

    Update swap.js

--- swap.js
+++ swap.js
@@ -1,19 +1,10 @@
-function swap(array, firstIndex, secondIndex) {
-    var temp = array[firstIndex];
-	array[firstIndex] = array[secondIndex];
-	array[secondIndex] = temp;
+function swap(arr, firstIndex, secondIndex) {
+    var temp = arr[firstIndex];
+	arr[firstIndex] = arr[secondIndex];
+	arr[secondIndex] = temp;
 };
 
 var testArray = [7, 9, 4];
 swap(testArray, 0, 1);
 
-println(testArray);
-
-Program.assertEqual(testArray, [9, 7, 4]);
-
-swap(testArray, 1, 2);
-Program.assertEqual(testArray, [9, 4, 7]);
-
-swap(testArray, 0, 2);
-Program.assertEqual(testArray, [7, 4, 9]);
-
+console.log(testArray);    // Should be [9, 7, 4] !
