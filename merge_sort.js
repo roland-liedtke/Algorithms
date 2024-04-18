a874500d9317fc38c61cd8d44a8cd02f2b7d0416
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-18 16:16:44 +0200

    Update merge_sort.js

--- merge_sort.js
+++ merge_sort.js
@@ -3,7 +3,29 @@
 var merge = function(array, p, q, r) {
     // This code has been purposefully obfuscated,
     //  as you'll write it yourself in next challenge.
-    var a=[],b=[],c=p,d,e;for(d=0;c<=q;d++,c++){a[d]=array[c];}for(e=0;c<=r;e++,c++){b[e]=array[c];}c=p;for(e=d=0;d<a.length&&e<b.length;){if(a[d]<b[e]){array[c]=a[d];d++;} else {array[c]=b[e]; e++;}c++; }for(;d<a.length;){array[c]=a[d];d++;c++;}for(;e<b.length;){array[c]=b[e];e++;c++;}
+    var a=[],b=[],c=p,d,e;
+    for(d=0;c<=q;d++,c++){
+        a[d]=array[c];
+    }
+    for(e=0;c<=r;e++,c++){
+        b[e]=array[c];
+    }
+    c=p;
+    for(e=d=0;d<a.length&&e<b.length;){
+        if(a[d]<b[e]){
+            array[c]=a[d];d++;
+        } else {
+            array[c]=b[e];
+            e++;
+        }
+        c++; 
+    }
+    for(;d<a.length;){
+        array[c]=a[d];d++;c++;
+    }
+    for(;e<b.length;){
+        array[c]=b[e];e++;c++;
+    }
 };
 
 
@@ -19,9 +41,4 @@ var mergeSort = function(array, p, r) {
 
 var array = [14, 7, 3, 12, 9, 11, 6, 2];
 mergeSort(array, 0, array.length-1);
-println("Array after sorting: " + array);
-Program.assertEqual(array, [2, 3, 6, 7, 9, 11, 12, 14]);
-
-var arr2 = [-3, 0, 4, 17, 22, 1];
-mergeSort(arr2, 0, arr2.length-1);
-Program.assertEqual(arr2, [-3,0,1,4,17,22]);
+console.log("Array after sorting: " + array);
