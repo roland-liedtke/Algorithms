dc9b185dfa0cc3973d619bbeb2d02d4443452383
Roland Liedtke <liedtke.roland@gmail.com>
2024-04-18 13:26:48 +0200

    Update and rename quickSort.js to quick_sort.js

--- quick_sort.js
+++ quick_sort.js
@@ -3,7 +3,19 @@
 var partition=function(array, p, r){
     // This code has been purposefully obfuscated,
     // as you will implement it yourself in next challenge
-    var e=array,t=p,n=r;var r=function(e,t,n){var r=e[t];e[t]=e[n];e[n]=r;};var i=t;for(var s=t;s<n;s++){if(e[s]<=e[n]){r(e,s,i);i++;}}r(e,n,i);return i;
+    var e=array,t=p,n=r;
+    var r=function(e,t,n){
+        var r=e[t];e[t]=e[n];e[n]=r;
+    };
+    var i=t;
+    for(var s=t;s<n;s++){
+        if(e[s]<=e[n]){
+            r(e,s,i);
+            i++;
+        }
+    }
+    r(e,n,i);
+    return i;
 };
 
 
